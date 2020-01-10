e802858a69c49909b13ece7297c8fae9768cf77e
Jeramy Dichiera <jeramy.dichiera@gmail.com>
2020-01-10 07:36:30 -0500

    Add tappy ship game activity and link up title button

--- app/src/main/java/com/example/gamelauncher/TappyShip.java
+++ app/src/main/java/com/example/gamelauncher/TappyShip.java
@@ -1,15 +1,27 @@
 package com.example.gamelauncher;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class TappyShip extends Activity {
+public class TappyShip extends Activity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tappy_ship);
+
+        final Button btnTappyShipPlay = (Button) findViewById(R.id.btnTappyShipPlay);
+
+        btnTappyShipPlay.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent tappyShipGameIntent = new Intent(this, TappyShipGameActivity.class);
+        startActivity(tappyShipGameIntent);
+        finish();
     }
 }
---
+++ app/src/main/java/com/example/gamelauncher/TappyShipGameActivity.java
@@ -0,0 +1,14 @@
+package com.example.gamelauncher;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TappyShipGameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tappy_ship_game);
+    }
+}
